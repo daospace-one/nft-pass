d36372e03a19a4e06f2b647f09a25caae3d29124
jiangplus <jiang.plus.times@gmail.com>
2022-10-18 19:09:45 +0800

    add mint script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,6 +4,23 @@ async function main() {
     const [owner] = await ethers.getSigners();
     const Mintable = await ethers.getContractFactory("SpacePassNFT");
     const mintable = await Mintable.connect(owner).deploy("SpacePassNFT", "SpacePassNFT", "https://daospace.one/nft/data/");
+    console.log("mintable.address", mintable.address);
+}
+
+
+async function mint() {
+    const [owner] = await ethers.getSigners()
+    const Mintable = await ethers.getContractFactory("SpacePassNFT")
+
+    const mintable = await Mintable.attach(process.env.CONTRASCT_ADDR)
+    console.log('SpacePassNFT address', mintable.address)
+    let user = process.env.USER_ADDR
+    console.log('user', user)
+    await mintable.mint(user, 3)
+    console.log(await mintable.ownerOf(1))
+    console.log(await mintable.connect(owner).activate(1))
+    console.log(await mintable.connect(owner).activated(1))
+    console.log(await mintable.lastActivated(user))
 }
 
 main().catch((error) => {
