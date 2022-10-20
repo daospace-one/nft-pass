ae77eeba3eadb000c1f8630047de7dc4581c3db1
jiangplus <jiang.plus.times@gmail.com>
2022-10-20 18:02:39 +0800

    add py sdk

--- scripts/sdk.py
+++ scripts/sdk.py
@@ -2,7 +2,6 @@ from web3 import Web3
 import json
 
 rpc_endpoint = "https://rpc.api.moonbeam.network"
-# rpc_endpoint = "http://127.0.0.1:8545"
 w3 = Web3(Web3.HTTPProvider(rpc_endpoint))
 
 abi_path = "artifacts/contracts/SpacePassNFT.sol/SpacePassNFT.json"
@@ -11,8 +10,9 @@ data = file.read()
 file.close()
 abi = json.loads(data)["abi"]
 
+address = "0x"
 # contract_address = "0x43f5bAc85d84f6cc4f4d838B1f523f511497973D"
-contract_address = "0x"
+contract_address = "0xce6685530FbA7cC34538149B2278e213Ce73FcDa"
 passnft = w3.eth.contract(
     address=contract_address,
     abi=abi
@@ -21,7 +21,7 @@ passnft = w3.eth.contract(
 print(passnft.functions.name().call())
 passnft.functions.ownerOf(1).call()
 passnft.functions.tokenURI(1).call()
-passnft.functions.passType(1).call()
+passnft.functions.duration(1).call()
 passnft.functions.expires(1).call()
 passnft.functions.activated(1).call()
-passnft.functions.lastActivated(address).call()
+print(passnft.functions.lastActivated(address).call())
